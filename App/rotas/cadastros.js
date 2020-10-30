0053cf842a3f23c896f8f3fe06bf9ab50b9443b4
Vinicius <vinny96santos@gmail.com>
2020-10-30 10:04:26 -0300

    antes de fazer as alterações de unificar tudo

--- App/rotas/cadastros.js
+++ App/rotas/cadastros.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../mysql/mysql')
+
 router.route('/produto')
     .get(function (req, res) {
-        knex.select().table('produtos').then((response) => {
-            res.send(response);
-        })
+        knex.select().table('produtos')
+            .then((response) => {
+                res.send(response);
+            })
+            .catch(err => {
+                res.send(err);
+            })
     });
 
 router.route('/fornecedor')
+    .get(function (req, res) {
+        knex.select().table('fornecedor')
+            .then((response) => {
+                res.send(response);
+            })
+            .catch(err => {
+                res.send(err);
+            })
+    })
     .post((req, res) => {
         knex("fornecedor").insert(req.body)
             .then(result => {
@@ -17,9 +31,26 @@ router.route('/fornecedor')
             .catch(err => {
                 res.send(err);
             })
-
+    })
+    .delete((req, res) => {
+        knex('fornecedor').where('CodFornecedor', req.body.CodFornecedor).del()
+            .then(() => {
+                res.send("Sucesso");
+            })
+            .catch(err => {
+                res.send(err);
+            })
     });
 router.route('/descricao')
+    .get(function (req, res) {
+        knex.select().table('descricao')
+            .then((response) => {
+                res.send(response);
+            })
+            .catch(err => {
+                res.send(err);
+            })
+    })
     .post((req, res) => {
         knex("descricao").insert(req.body)
             .then(result => {
@@ -29,7 +60,15 @@ router.route('/descricao')
                 res.send(err);
             })
 
+    })
+    .delete((req, res) => {
+        knex('descricao').where('CodDescricao', req.body.CodDescricao).del()
+            .then(() => {
+                res.send("Sucesso");
+            })
+            .catch(err => {
+                res.send(err);
+            })
+
     });
-
-
 module.exports = router;
